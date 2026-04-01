b16eb08197bd2b4fece3e3b12cd803c13844871b
Rinky Kumari <iamrinkykumari@gmail.com>
2026-04-02 00:16:28 +0530

    reset admin password

--- Desktop/cctv_website/build.sh
+++ Desktop/cctv_website/build.sh
@@ -4,6 +4,6 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin','admin@gmail.com','Admin@123')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); u=User.objects.get(username='admin'); u.set_password('Admin@123'); u.save()" | python manage.py shell
 
 python manage.py collectstatic --noinput
