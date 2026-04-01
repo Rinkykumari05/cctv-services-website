728c2d055a1b30d0e633e5d0da88cbc473347123
Rinky Kumari <iamrinkykumari@gmail.com>
2026-04-01 23:30:42 +0530

    Create admin automatically on deploy

--- Desktop/cctv_website/build.sh
+++ Desktop/cctv_website/build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
 pip install -r requirements.txt
-
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model;
-User = get_user_model();
-User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'servizable@gmail.com', 'Admin@12345')" | python manage.py shell
 
 python manage.py collectstatic --noinput
