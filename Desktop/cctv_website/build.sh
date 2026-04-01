6948ad09b34f6720cc7bfbb6d4ed04e2e7ca449c
Rinky Kumari <iamrinkykumari@gmail.com>
2026-04-01 22:54:21 +0530

    auto create superuser fix

--- Desktop/cctv_website/build.sh
+++ Desktop/cctv_website/build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
+
+python manage.py collectstatic --noinput
